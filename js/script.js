3a6ebc4ac8c8b3f513d70096f320df954cdb733f
Serhiy <coddign@gmail.com>
2023-05-05 18:11:11 +0300

    fist commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+// const a = 5;
+// const b = 7;
+
+// function test() {
+//     alert(1);
+// }
+
+// alert('Hi');
+// const a = confirm('M?');
+// console.log(a);
+
+const answer = prompt('18?',  '');
+console.log(answer);
+
+
+console.log(typeof(null));
